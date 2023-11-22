e11125865aafa76dc784d8afe80c64a102582c06
TienVanThuan <tienvanthuan.dev@gmail.com>
2023-11-22 14:03:07 +0700

    update breadcrumb template

--- root/wp-content/themes/wp/functions/other.php
+++ root/wp-content/themes/wp/functions/other.php
@@ -80,3 +80,62 @@ function my_acf_op_init() {
 
 /* Disable WordPress Admin Bar for all users */
 add_filter( 'show_admin_bar', '__return_false' );
+
+
+//breadcrumb
+// calling the_breadcrumb the_breadcrumb();
+function the_breadcrumb() {
+
+  $sep = ' > ';
+
+  if (!is_front_page()) {
+
+      echo '<div class="breadcrumbs">';
+      echo '<a href="';
+      echo get_option('home');
+      echo '">';
+      bloginfo('name');
+      echo '</a>' . $sep;
+
+
+      if (is_category() || is_single() ){
+          the_category('title_li=');
+      } elseif (is_archive() || is_single()){
+          if ( is_day() ) {
+              printf( __( '%s', 'twentynov' ), get_the_date() );
+          } elseif ( is_month() ) {
+              printf( __( '%s', 'twentynov' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentynov' ) ) );
+          } elseif ( is_year() ) {
+              printf( __( '%s', 'twentynov' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentynov' ) ) );
+          } else {
+              _e( 'Blog Archives', 'twentynov' );
+          }
+      }
+
+
+      if (is_single()) {
+          echo $sep;
+          the_title();
+      }
+
+
+      if (is_page()) {
+          echo the_title();
+      }
+
+
+      if (is_home()){
+          global $post;
+          $page_for_posts_id = get_option('page_for_posts');
+          if ( $page_for_posts_id ) { 
+              $post = get_post($page_for_posts_id);
+              setup_postdata($post);
+              the_title();
+              rewind_posts();
+          }
+      }
+
+      echo '</div>';
+  }
+}
+?>
